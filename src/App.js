8a0b9fc313e84cf42d12bd6bf6f454451576f01b
Andrey Shoful <shoful.a@gmail.com>
2021-09-08 13:38:38 +0300

    add TextFields to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import TextFields from "./Components/TextFields/TextFields";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Apps</header>
+      <header className="App-header">
+        <TextFields />
+      </header>
+      {/* <span className="material-icons">play_circle_outline</span>
+      <span className="material-icons">play_circle</span>
+      <span className="material-icons">manage_search</span>
+      <span className="material-icons">keyboard_arrow_up</span>
+      <span className="material-icons">keyboard_arrow_down</span> */}
     </div>
   );
 }
---
+++ src/Components/TextFields/TextFields.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addNote } from "../../redux/action/action";
+import "./TextFields.css";
+
+const TextFields = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      id: Date.now(),
+      title,
+      description,
+    };
+    dispatch(addNote(data));
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <div className="TextFields">
+      <input className="TextFields_input" type="text" />
+      <form className="TextFields_form" onSubmit={handleSubmit}>
+        <div className="TextFields_div">
+          <p>Title</p>
+          <span className="material-icons">delete</span>
+        </div>
+        <input
+          className="TextFields_input"
+          type="text"
+          // placeholder="title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <p>Description</p>
+        <textarea
+          className="TextFields_textarea"
+          rows="10"
+          type="text"
+          // placeholder="description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <button className="TextFields_button" onClick={handleSubmit}>
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TextFields;
